cda5edcbd32dce9350d05d489866bea34189f428
Makuo <ilekubamakuochukwu@gmail.com>
2024-06-12 15:23:17 +0100

    Second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHello2(): { message: string, title: string } {
+    return { message: 'Hello World!', title: 'My Greeting App' };
+  }
+
+  getHome(): { title: string } {
+    return { title: 'Home Page' };
+  }
+
+  getAboutUs(): { title: string } {
+    return { title: 'About Us Page' };
+  }
 }
